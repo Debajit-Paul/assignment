47e1a6acc6e74a7695f5060a9f40807e0227939a
Debajit Paul <62774980+Debajit-Paul@users.noreply.github.com>
2024-06-15 21:45:13 +0530

    redux persist removed

--- Frontend/src/redux/store.ts
+++ Frontend/src/redux/store.ts
@@ -1,23 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import carReducer from "./feature/carSlice";
-import storage from "redux-persist/lib/storage";
-import { persistReducer } from "redux-persist";
-import { combineReducers } from "@reduxjs/toolkit";
-
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
-
-const reducer = combineReducers({
-  car: carReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    car: carReducer,
+  },
 });
 
 export default store;
